7738fb76e39556e3593df4fc4614ef9adfcc6000
Wighawag <wighawag@gmail.com>
2019-09-17 21:26:58 +0100

    cleanup

--- khafile.js
+++
@@ -1,6 +0,0 @@
-let project = new Project('The Ethernal Dungeon');
-project.addAssets('Assets/**');
-project.addShaders('Shaders/**');
-project.addSources('Sources');
-project.addLibrary('base');
-resolve(project);
--- webapp/src/stores/dungeon.js
+++ webapp/src/stores/dungeon.js
@@ -2,7 +2,7 @@ import { writable, readable, derived } from 'svelte/store';
 import Web3 from 'web3';
 import Dungeon from '../../../contracts/dungeon';
 import BN from 'bn.js';
-import {pause} from '../utils/time';
+import { pause } from '../utils/time';
 import NiftyGatewayJS from 'niftygateway';
 import Portis from '@portis/web3';
 import { Bitski, AuthenticationStatus } from 'bitski';
@@ -102,12 +102,12 @@ function log(channel, ...args) {
 
 // dec2hex :: Integer -> String
 // i.e. 0-255 -> '00'-'ff'
-function dec2hex (dec) {
+function dec2hex(dec) {
 	return ('0' + dec.toString(16)).substr(-2)
 }
-  
+
 // generateRandomKey :: Void -> String
-function generateRandomKey () {
+function generateRandomKey() {
 	var arr = new Uint8Array(32);
 	crypto.getRandomValues(arr);
 	return Array.from(arr, dec2hex).join('')
@@ -117,75 +117,75 @@ function generateRandomKey () {
 async function fetchChainInfos() {
 	const validChains = [];
 	let contracts;
-	try{
+	try {
 		const response = await axios.get('contracts');
 		contracts = response.data;
-		for(let chainId of Object.keys(contracts)) {
+		for (let chainId of Object.keys(contracts)) {
 			validChains.push(chainId);
 		}
-	} catch(e) {
+	} catch (e) {
 		console.error('no contracts configured');
 	}
-	return {validChains, contracts};
+	return { validChains, contracts };
 }
 
 function getProviderType(provider) {
 	if (provider.isMetaMask)
-        return 'metamask';
+		return 'metamask';
 
-    if (provider.isTrust)
-        return 'trust';
+	if (provider.isTrust)
+		return 'trust';
 
-    if (provider.isGoWallet)
-        return 'goWallet';
+	if (provider.isGoWallet)
+		return 'goWallet';
 
-    if (provider.isAlphaWallet)
-        return 'alphaWallet';
+	if (provider.isAlphaWallet)
+		return 'alphaWallet';
 
-    if (provider.isStatus)
-        return 'status';
+	if (provider.isStatus)
+		return 'status';
 
-    if (provider.isToshi)
-        return 'coinbase';
+	if (provider.isToshi)
+		return 'coinbase';
 
-    if (typeof window.__CIPHER__ !== 'undefined')
-        return 'cipher';
+	if (typeof window.__CIPHER__ !== 'undefined')
+		return 'cipher';
 
-    if (provider.constructor.name === 'EthereumProvider')
-        return 'mist';
+	if (provider.constructor.name === 'EthereumProvider')
+		return 'mist';
 
-    if (provider.constructor.name === 'Web3FrameProvider')
-        return 'parity';
+	if (provider.constructor.name === 'Web3FrameProvider')
+		return 'parity';
 
-    if (provider.host && provider.host.indexOf('infura') !== -1)
-        return 'infura';
+	if (provider.host && provider.host.indexOf('infura') !== -1)
+		return 'infura';
 
-    if (provider.host && provider.host.indexOf('localhost') !== -1)
-        return 'localhost';
+	if (provider.host && provider.host.indexOf('localhost') !== -1)
+		return 'localhost';
 }
 
 async function generateWeb3Status(web3, providerUsed, checkAccounts = true) {
-	if(providerUsed) {
-		localStorage.setItem('lastProvider', JSON.stringify(providerUsed)); 
+	if (providerUsed) {
+		localStorage.setItem('lastProvider', JSON.stringify(providerUsed));
 	}
-	const {validChains, contracts} = await fetchChainInfos();
+	const { validChains, contracts } = await fetchChainInfos();
 	let validChain = false;
 	let chainId;
-	try{
+	try {
 		chainId = await web3.eth.net.getId();
-	} catch(e) {
+	} catch (e) {
 	}
-	
+
 	let config;
-	if(chainId) {
-		if(validChains.indexOf('' + chainId) != -1) {
+	if (chainId) {
+		if (validChains.indexOf('' + chainId) != -1) {
 			validChain = true;
 			DungeonInfo = contracts[chainId]['Dungeon'];
 		}
 		config = require('../../../config')(chainId)
-		if(checkAccounts) {
+		if (checkAccounts) {
 			const accounts = await web3.eth.getAccounts();
-			if(accounts && accounts.length > 0) {
+			if (accounts && accounts.length > 0) {
 				//console.log('accounts available : ' + accounts[0]);
 				return {
 					available: true,
@@ -201,7 +201,7 @@ async function generateWeb3Status(web3, providerUsed, checkAccounts = true) {
 			//console.log('account not available');
 		}
 	}
-	
+
 	return {
 		available: true,
 		validChain,
@@ -218,40 +218,40 @@ export const web3Status = (() => {
 	const { subscribe, set, update } = writable();
 
 	set("loading");
-    window.addEventListener('load', async () => {
+	window.addEventListener('load', async () => {
 		//console.log('page loaded');
-		try{
+		try {
 			$web3Status = await loadWeb3Status();
 			set($web3Status);
-			if($web3Status.enabled && $web3Status.validChain) {
+			if ($web3Status.enabled && $web3Status.validChain) {
 				log('dungeon', 'loading')
 				dungeon.load();
 			}
-		} catch(e) {
+		} catch (e) {
 			// TODO remove and ensure ti never reach there
 			console.error('LOAD STATUS ERROR', e);
-			set({error:e});
+			set({ error: e });
 		}
-	});	
+	});
 
 	async function loadWeb3Status() {
 		const lastProviderUsedS = localStorage.getItem('lastProvider');
-		if(lastProviderUsedS) {
+		if (lastProviderUsedS) {
 			const lastProviderUsed = JSON.parse(lastProviderUsedS);
-			console.log({lastProviderUsed})
-			if(lastProviderUsed.type == 'portis') {
+			console.log({ lastProviderUsed })
+			if (lastProviderUsed.type == 'portis') {
 				console.log('using portis');
 				return setupPortis(lastProviderUsed.params);
-			} else if(lastProviderUsed.type == 'bitski') {
+			} else if (lastProviderUsed.type == 'bitski') {
 				console.log('using bitski');
 				return setupBitski(lastProviderUsed.params);
-			}else{
+			} else {
 				return setupBuiltInWallet();
 			}
 		}
 
-		const {validChains, contracts} = await fetchChainInfos();
-		const {provider, checkAccounts, providerType} = getBuiltInProvider();
+		const { validChains, contracts } = await fetchChainInfos();
+		const { provider, checkAccounts, providerType } = getBuiltInProvider();
 		return {
 			firstTime: true,
 			available: !!provider,
@@ -270,13 +270,13 @@ export const web3Status = (() => {
 		const web3 = new Web3(portis.provider);
 		window.web3 = web3;
 		window.portis = portis;
-		return generateWeb3Status(web3, {type:'portis', params: chainName});
+		return generateWeb3Status(web3, { type: 'portis', params: chainName });
 	}
 
 	async function usePortis(chainName) {
 		$web3Status = await setupPortis(chainName)
 		set($web3Status)
-		if($web3Status.validChain) {
+		if ($web3Status.validChain) {
 			await dungeon.load();
 		} else {
 			console.log('WRONG CHAIN IN PORTIS')
@@ -292,17 +292,17 @@ export const web3Status = (() => {
 		window.bitski = bitski;
 		if (bitski.authStatus === AuthenticationStatus.Connected) {
 			console.log('already connected');
-			return generateWeb3Status(web3, {type:'bitski', params: chainName});	
-		}else if(bitski.authStatus === AuthenticationStatus.NotConnected){
+			return generateWeb3Status(web3, { type: 'bitski', params: chainName });
+		} else if (bitski.authStatus === AuthenticationStatus.NotConnected) {
 			console.log('bitski not connected');
-		}else if(bitski.authStatus === AuthenticationStatus.Expired) {
+		} else if (bitski.authStatus === AuthenticationStatus.Expired) {
 			console.log('bitski expired');
 		} else {
 			console.error('wrong connection status', bitski.authStatus);
 		}
-		
-		const {validChains, contracts} = await fetchChainInfos();
-		const {provider, checkAccounts, providerType} = getBuiltInProvider();
+
+		const { validChains, contracts } = await fetchChainInfos();
+		const { provider, checkAccounts, providerType } = getBuiltInProvider();
 		return {
 			firstTime: true,
 			available: !!provider,
@@ -313,12 +313,12 @@ export const web3Status = (() => {
 
 	async function useBitski(chainName) {
 		$web3Status = await setupBitski(chainName)
-		if(!$web3Status.enabled) {
+		if (!$web3Status.enabled) {
 			await bitski.signIn(); // force popup
-			$web3Status = await generateWeb3Status(web3, {type:'bitski', params: chainName});
+			$web3Status = await generateWeb3Status(web3, { type: 'bitski', params: chainName });
 		}
 		set($web3Status)
-		if($web3Status.validChain) {
+		if ($web3Status.validChain) {
 			await dungeon.load();
 		} else {
 			console.log('WRONG CHAIN IN BITSKI')
@@ -333,23 +333,23 @@ export const web3Status = (() => {
 		if (window.ethereum) {
 			provider = window.ethereum;
 			providerType = getProviderType(window.ethereum);
-			
-			if(typeof providerType == 'undefined') {
+
+			if (typeof providerType == 'undefined') {
 				checkAccounts = false;// TODO only for navigator.userAgent.indexOf("Opera");
 				console.log('unknown provider type');
 			} else {
-				console.log({providerType});
+				console.log({ providerType });
 			}
 		} else if (window.web3) {
 			provider = window.web3.currentProvider;
 		}
-		return {provider, checkAccounts, providerType};
+		return { provider, checkAccounts, providerType };
 	}
 
 	async function setupBuiltInWallet() {
-		
-		const {provider, checkAccounts, providerType} = getBuiltInProvider();
-		if(!provider){
+
+		const { provider, checkAccounts, providerType } = getBuiltInProvider();
+		if (!provider) {
 			return {
 				available: false,
 			}
@@ -357,13 +357,13 @@ export const web3Status = (() => {
 
 		const web3 = new Web3(provider);
 		window.web3 = web3;
-		return generateWeb3Status(web3, {type: providerType}, checkAccounts);
+		return generateWeb3Status(web3, { type: providerType }, checkAccounts);
 	}
 
 	async function useBuiltInWallet() {
 		$web3Status = await setupBuiltInWallet()
 		set($web3Status)
-		if($web3Status.validChain) {
+		if ($web3Status.validChain) {
 			await dungeon.load();
 		} else {
 			// TODO allow to manually switch for portis
@@ -373,7 +373,7 @@ export const web3Status = (() => {
 	async function useNiftyGateway() {
 		$web3Status = await setupNiftyGateway()
 		set($web3Status)
-		if($web3Status.validChain) {
+		if ($web3Status.validChain) {
 			await dungeon.load();
 		} else {
 			// TODO allow to manually switch for portis
@@ -388,7 +388,7 @@ export const web3Status = (() => {
 		console.log(nftg);
 		const nftgUser = await nftg.getWalletAndEmailAddress(); //didSucceed, emailAddress, walletAddress
 		console.log(nftgUser);
-		if(nftgUser.didSucceed) {
+		if (nftgUser.didSucceed) {
 			$web3Status.enabled = true;
 			$web3Status.available = true;
 			$web3Status.account = nftgUser.walletAddress;
@@ -397,31 +397,31 @@ export const web3Status = (() => {
 		return $web3Status;
 	}
 
-    async function enable() {
-		try{
+	async function enable() {
+		try {
 			log('wallet', 'enabling');
 			$web3Status.enabling = true;
 			set($web3Status); // TODO set({enabling:true});
 			let accounts = await web3.eth.getAccounts();
 			log('wallet', 'getAccounts', accounts);
-			if(!accounts || accounts.length == 0) {	
-				try{
+			if (!accounts || accounts.length == 0) {
+				try {
 					accounts = await web3.currentProvider.enable();
 					log('wallet', 'enable', accounts);
-				} catch(e) {
+				} catch (e) {
 					log('wallet', 'enable rejection');
 				}
-				
+
 				// Metamask no privacy allow you to fetch account here even though the user did not enable
-				if(!accounts || accounts.length == 0) {
+				if (!accounts || accounts.length == 0) {
 					log('wallet', 'no accounts, fetching...');
 					const testAccounts = await web3.eth.getAccounts();
 					log('wallet', 'getAccounts again', testAccounts);
 					accounts = testAccounts; // TODO remove ?
 				}
 			}
-			
-			if(accounts && accounts.length > 0) {
+
+			if (accounts && accounts.length > 0) {
 				log('wallet', 'enabled account : ' + accounts[0]);
 				$web3Status.enabled = true;
 				$web3Status.account = accounts[0];
@@ -430,68 +430,68 @@ export const web3Status = (() => {
 				$web3Status.enabled = false;
 			}
 			$web3Status.enabling = false;
-		} catch(e) {
+		} catch (e) {
 			console.error('ENABLING ERROR', e);
 			$web3Status.enabling = false;
 			// throw new Error("not enabled");
 		}
 		set($web3Status);
-    }
-    return { enable, subscribe, useNiftyGateway, usePortis, useBitski, useBuiltInWallet };
+	}
+	return { enable, subscribe, useNiftyGateway, usePortis, useBitski, useBuiltInWallet };
 })()
 
 
 export const dungeon = (() => {
 	const { subscribe, set, update } = writable();
 
-    async function load() {
+	async function load() {
 		set("loading");
-        try{
-            const dungeon = await loadDungeon();
+		try {
+			const dungeon = await loadDungeon();
 			set(dungeon);
-		} catch(e) {
+		} catch (e) {
 			console.error(e);
-			set({error:e});
+			set({ error: e });
 		}
-    }
-    return { load, subscribe };
+	}
+	return { load, subscribe };
 })()
 
 
 async function loadDungeon() {
-	
-	if(!($web3Status.available)) {
+
+	if (!($web3Status.available)) {
 		throw new Error('web3 not available');
 	}
 	const web3 = $web3Status.web3;
 
-	
-	if(!$web3Status.enabled){
-		try{
+
+	if (!$web3Status.enabled) {
+		try {
 			await web3Status.enable();
-		} catch(e) {
+		} catch (e) {
 			throw new Error('web3 enabling error');
 		}
-		if(!$web3Status.enabled) {
+		if (!$web3Status.enabled) {
 			console.log('still not enabled');
 			return;
 		}
 	}
 
 	const player = $web3Status.account.toLowerCase();
-	console.log({player});
+	console.log({ player });
 
 	let key;
 	const dataS = localStorage.getItem(player); // TODO on accounts changed, need to reset // easiest solution : reload page on account change
 	let data;
-	if(dataS) {
+	if (dataS) {
 		data = JSON.parse(dataS);
 	}
-	if(!data || !data.key) {
+	if (!data || !data.key) {
 		key = generateRandomKey();
 		try {
-			localStorage.setItem(player, JSON.stringify({key}));
-		} catch(e) {
+			localStorage.setItem(player, JSON.stringify({ key }));
+		} catch (e) {
 			console.error('could not save to storage');
 		}
 		// TODO remove
@@ -501,18 +501,18 @@ async function loadDungeon() {
 		// TODO remove
 		console.log('reusing : ', key);
 	}
-	
+
 	const dungeon = new Dungeon(web3.currentProvider, DungeonInfo.address, DungeonInfo.contractInfo.abi, {
-        logLevel: 'trace',
+		logLevel: 'trace',
 		blockInterval: 12,
 		price: $web3Status.config.price,
-    });
+	});
 	await dungeon.init(player, key);  // TODO on accounts changed, need to reset // easiest solution : reload page on account change
 
 	window.web3 = web3; // DEBUGING
 	window.BN = BN;
 	window.dungeon = dungeon;
-	
+
 	return dungeon;
 }
 
@@ -536,54 +536,54 @@ async function loadDungeon() {
 // })
 
 function deriveFromDungeon(event, fieldName) {
-    return derived(dungeon, ($dungeon, set) => {
-        if($dungeon && !$dungeon.error && $dungeon != "loading") {
-            set($dungeon[fieldName]);
-            $dungeon.on(event, set);
-        }
-    }, null);
+	return derived(dungeon, ($dungeon, set) => {
+		if ($dungeon && !$dungeon.error && $dungeon != "loading") {
+			set($dungeon[fieldName]);
+			$dungeon.on(event, set);
+		}
+	}, null);
 }
 
 export const playerLocation = deriveFromDungeon('playerMoved', 'playerLocation');
 export const playerEnergy = deriveFromDungeon('energyChanged', 'energy');
 
 export const newAddress = derived(dungeon, ($dungeon, set) => {
-    if($dungeon && !$dungeon.error && $dungeon != "loading") {
-        set(!$dungeon.isCurrentDelegate);
-        $dungeon.on('delegated', (delegate) => { set(false); });
-    }
+	if ($dungeon && !$dungeon.error && $dungeon != "loading") {
+		set(!$dungeon.isCurrentDelegate);
+		$dungeon.on('delegated', (delegate) => { set(false); });
+	}
 }, null);
 
 export const playerInDungeon = derived(dungeon, ($dungeon, set) => {
-    if($dungeon && !$dungeon.error && $dungeon != "loading") {
-        set($dungeon.inDungeon);
-        $dungeon.on('playerEntered', () => { set(true)});
-        $dungeon.on('playerQuit', () => { set(false)});
-    }
+	if ($dungeon && !$dungeon.error && $dungeon != "loading") {
+		set($dungeon.inDungeon);
+		$dungeon.on('playerEntered', () => { set(true) });
+		$dungeon.on('playerQuit', () => { set(false) });
+	}
 }, null);
 
 export const roomBlockUpdate = derived(dungeon, ($dungeon, set) => {
-	$dungeon.on('roomChanged', (block) => { set(block)}); // TODO roomChanged for new exits (from neighbor rooms)
+	$dungeon.on('roomChanged', (block) => { set(block) }); // TODO roomChanged for new exits (from neighbor rooms)
 })
 
 
 function textify(room) {
 	function textifyScene(scene, directions) { // origin
 		let description = scene.description;
-		if(directions) {
+		if (directions) {
 			const exitsArray = [];
-			for(let exit of ['north', 'east', 'south', 'west']) {
-				if(directions[exit]) {
+			for (let exit of ['north', 'east', 'south', 'west']) {
+				if (directions[exit]) {
 					exitsArray.push(exit);
 				}
 			}
 			let exitsDescription;
-			if(exitsArray.length == 0) {
+			if (exitsArray.length == 0) {
 				exitsDescription = "The room has no exits";
-			} else if(exitsArray.length == 1) {
+			} else if (exitsArray.length == 1) {
 				exitsDescription = "The room has only one exists, on the " + exitsArray[0];
 			} else {
-				exitsDescription = 'There are ' + exitsArray.length + ' exists, on the ' + exitsArray.slice(0, exitsArray.length-1).join(', ') + ' and ' + exitsArray[exitsArray.length-1];
+				exitsDescription = 'There are ' + exitsArray.length + ' exists, on the ' + exitsArray.slice(0, exitsArray.length - 1).join(', ') + ' and ' + exitsArray[exitsArray.length - 1];
 			}
 			description.push(exitsDescription)
 		}
@@ -595,14 +595,14 @@ function textify(room) {
 }
 
 export const room = derived([dungeon, playerLocation, roomBlockUpdate], ([$dungeon, $playerLocation, $roomBlockUpdate], set) => {
-	if($playerLocation && $dungeon && !$dungeon.error) {
+	if ($playerLocation && $dungeon && !$dungeon.error) {
 		const room = $dungeon.rooms[$playerLocation];
 		// console.log({room});
 		const $directions = $dungeon.allExitsFor(room);
 		let roomDesc;
-		if(room.location == '0') {
+		if (room.location == '0') {
 			console.log('HALL');
-			roomDesc = hallDesc;	
+			roomDesc = hallDesc;
 		} else {
 			roomDesc = rooms[$dungeon.getRandomValue(room.location, room.hash, 99, rooms.length)];
 		}
